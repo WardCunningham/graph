94b597575fe5a43931293e76c50d84ef94d740b5
Ward Cunningham <ward@c2.com>
2022-02-19 14:50:22 -0800

    Create model.js
    Adapted from spike implementation
    http://ward.dojo.fed.wiki/transform-to-nodes-and-rels.html

---
+++ src/model.js
@@ -0,0 +1,17 @@
+  export let nodes = []
+  export let rels = []
+
+  export function addnode(type, props) {
+    let obj = {type, in:[], out:[], props}
+    nodes.push(obj)
+    return nodes.length-1
+  }
+
+  export function addrel(type, from, to, props={}) {
+    let obj = {type, from, to, props}
+    rels.push(obj)
+    let rid = rels.length-1
+    nodes[from].out.push(rid)
+    nodes[to].in.push(rid)
+    return rid
+  }
