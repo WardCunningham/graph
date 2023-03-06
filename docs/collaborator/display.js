c9d492abe5e4ac59d457bb1f6afc11e1bc9c057b
Robert Best <chessscholar@gmail.com>
2023-03-06 00:54:09 -0500

    Changing to latest hpcc graphviz module

--- docs/collaborator/display.js
+++ docs/collaborator/display.js
@@ -5,7 +5,9 @@ const panZoom = {}
 import {composite} from './composite.js'
 import {dotify} from './dotify.js'
 import {hoverbold} from './hoverbold.js'
+import {Graphviz} from "https://unpkg.com/@hpcc-js/wasm@2.8.0/dist/graphviz.js"
 
+const graphviz = await Graphviz.load()
 
 export async function display(chosen,target) {
   let targetsvg = null
@@ -24,30 +26,29 @@ export async function display(chosen,target) {
       }
       const dot = dotify(complex)
       window.dot = dot
-      hpccWasm.graphviz.layout(window.dot, "svg", "dot").then(svg => {
-        target.innerHTML = svg;
-        targetsvg = target.querySelector('svg')
+      const svg = graphviz.layout(window.dot, "svg", "dot")
+      target.innerHTML = svg;
+      targetsvg = target.querySelector('svg')
 
-        drawing = false
-        hoverbold(target)
-        const targetBounds = { width: target.clientWidth, height: target.clientHeight } 
-        const svgBounds = { width: targetsvg.clientWidth, height: targetsvg.clientHeight }
-        let svgElement = targetsvg
-        panSVG = svgPanZoom(svgElement)
-        targetsvg.style.height = "100%"
-        targetsvg.style.width = "100%"
-        if (targetBounds.width < svgBounds.width || targetBounds.height < svgBounds.height) {
-          panSVG.resize()
-        }
-        panSVG.fit()
-        panSVG.center()
-        if (panZoom.size && 
-            panZoom.size.width == targetsvg.width.baseVal.valueInSpecifiedUnits &&
-            panZoom.size.height == targetsvg.height.baseVal.valueInSpecifiedUnits) {
-          panSVG.zoom(panZoom.zoom)
-          panSVG.pan(panZoom.pan)
-        }
-      })
+      drawing = false
+      hoverbold(target)
+      const targetBounds = { width: target.clientWidth, height: target.clientHeight } 
+      const svgBounds = { width: targetsvg.clientWidth, height: targetsvg.clientHeight }
+      let svgElement = targetsvg
+      panSVG = svgPanZoom(svgElement)
+      targetsvg.style.height = "100%"
+      targetsvg.style.width = "100%"
+      if (targetBounds.width < svgBounds.width || targetBounds.height < svgBounds.height) {
+        panSVG.resize()
+      }
+      panSVG.fit()
+      panSVG.center()
+      if (panZoom.size && 
+          panZoom.size.width == targetsvg.width.baseVal.valueInSpecifiedUnits &&
+          panZoom.size.height == targetsvg.height.baseVal.valueInSpecifiedUnits) {
+        panSVG.zoom(panZoom.zoom)
+        panSVG.pan(panZoom.pan)
+      }
     } catch (err) {
       console.log('display error', err)
       drawing = false
